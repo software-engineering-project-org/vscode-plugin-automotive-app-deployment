20553b10aa0867a6155afc4c40c50d4f1bd068ec
Carlo Pötter <carlo.poetter@bertelsmann.de>
2023-05-31 21:59:11 +0200

    change location for templates

--- src/test/jest/manifestGenerator.test.js
+++ src/test/jest/manifestGenerator.test.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
-const path = require('path');
 const { ManifestGeneratorJson } = require('../../svc/ManifestGeneratorJson');
 
-describe('ManifestGeneratorJsonTemplatev1', () => {
-  const templateFilePath = 'templates/kanto/kanto_container_conf_template_v1.json';
-  const outputFilePath = 'tmp/tmp_gen_kanto_container_manifest_v1.json';
+describe('ManifestGeneratorJsonTemplate', () => {
+  const templateFilePath = '.vscode/templates/kanto_container_conf_template.json';
+  const outputFilePath = '.vscode/tmp/tmp_gen_kanto_container_manifest.json';
 
   beforeEach(() => {
     // Clean up the output file before each test
@@ -13,42 +12,7 @@ describe('ManifestGeneratorJsonTemplatev1', () => {
     }
   });
 
-  test('generates the modified Kanto Container manifest (v1 template)', () => {
-    const keyValuePairs = {
-      'container_name': 'changed-container-name',
-      'image.name': 'changed-the-name-for-image'
-    };
-
-    const generator = new ManifestGeneratorJson(templateFilePath, outputFilePath);
-
-    // Generate the modified Kanto Container manifest
-    generator.generateKantoContainerManifest(keyValuePairs);
-
-    // Verify that the output file exists
-    expect(fs.existsSync(outputFilePath)).toBe(true);
-
-    // Read the generated JSON file
-    const generatedJson = fs.readFileSync(outputFilePath, 'utf8');
-    const parsedJson = JSON.parse(generatedJson);
-
-    // Verify that the modified values are correctly set in the generated JSON
-    expect(parsedJson.container_name).toBe('changed-container-name');
-    expect(parsedJson.image.name).toBe('changed-the-name-for-image');
-  });
-});
-
-describe('ManifestGeneratorJsonTemplatev2', () => {
-  const templateFilePath = 'templates/kanto/kanto_container_conf_template_v2.json';
-  const outputFilePath = 'tmp/tmp_gen_kanto_container_manifest_v2.json';
-
-  beforeEach(() => {
-    // Clean up the output file before each test
-    if (fs.existsSync(outputFilePath)) {
-      fs.unlinkSync(outputFilePath);
-    }
-  });
-
-  test('generates the modified Kanto Container manifest (v2 template)', () => {
+  test('generates the modified Kanto Container manifest', () => {
     const keyValuePairs = {
       'id': 'changed-container-id',
       'name': 'changed-container-name',
