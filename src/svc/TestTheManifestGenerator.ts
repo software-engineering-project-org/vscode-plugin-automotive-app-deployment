ed0fb8181c73f1a985112027e5ca8d0bffdeed73
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-05-31 10:53:15 +0200

    #test Instance the class for function demonstration

---
+++ src/svc/TestTheManifestGenerator.ts
@@ -0,0 +1,23 @@
+import { ManifestGenerator } from './TemplateGenerator';
+
+class TestTheManifestGenerator {
+  constructor() {
+    const inputDir = 'templates/kanto';
+    const inputFile = 'kanto_container_conf_template.json';
+    const outputDir = 'tmp';
+    const outputFile = 'tmp_gen_kanto_container_manifest.json';
+
+    const templateFilePath = `${inputDir}/${inputFile}`;
+    const outputFilePath = `${outputDir}/${outputFile}`;
+
+    const manifestGenerator = new ManifestGenerator(templateFilePath, outputFilePath);
+
+    // Example usage: Generating a modified Kanto Container manifest
+    manifestGenerator.generateKantoContainerManifest({
+      'container_name': 'changed-container-name',
+      'image.name': 'changed-the-name-for-image'
+    });
+  }
+}
+
+new TestTheManifestGenerator();
