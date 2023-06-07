9b47af43440e17e7aed92e5e6e11e3b0a33ad180
Carlo Pötter <carlo.poetter@bertelsmann.de>
2023-06-07 15:22:16 +0200

    begin imp for stage one deployment

--- src/cmd/DeploymentCommands.ts
+++ src/cmd/DeploymentCommands.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { LedaDeviceTreeItem } from '../provider/DeviceDataProvider';
 import { getTargetDeviceWithQuickPick } from './DeviceCommands';
 import { LedaDevice } from '../interfaces/LedaDevice';
-
+import { ManifestGeneratorJson } from '../svc/ManifestGeneratorJson';
 
 export async function deployApplication(item: LedaDeviceTreeItem) {
   let device = item?.ledaDevice;
@@ -12,6 +12,17 @@ export async function deployApplication(item: LedaDeviceTreeItem) {
       device = quickPickResult as LedaDevice;
     }
   }
+
+  const deployerStage1 = new ManifestGeneratorJson(
+    "/mnt/c/Users/POET038/VSCode/vscode-plugin-automotive-app-deployment/templates/kanto_container_conf_template.json",
+    "/mnt/c/Users/POET038/VSCode/vscode-plugin-automotive-app-deployment/tmp/out.json"
+    )
+
+  await deployerStage1.generateKantoContainerManifest(
+    {
+      'image.name': 'ghcr.io/sampleapp/test:latest'
+    }
+  )
 
   vscode.window.showInformationMessage(`Deploying to ${device.name}`);
 }
