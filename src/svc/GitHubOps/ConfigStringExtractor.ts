dc715f4c6f60662766c0c9528c0bb45d80e389b6
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-06-17 02:22:03 +0200

    #fix Captured groups call of regex match

--- src/svc/GitHubOps/ConfigStringExtractor.ts
+++ src/svc/GitHubOps/ConfigStringExtractor.ts
@@ -11,7 +11,7 @@ export class ConfigStringExtractor {
    * @throws {Error} If the .git/config file is not found, remote origin URL is missing, or the URL is invalid.
    */
   public static extractGitOrgAndRepoNameFromConfig = async (): Promise<string> => {
-    const gitConfigPath = path.resolve('../../../.git/config');
+    const gitConfigPath = path.resolve('.git/config');
 
     try {
       const gitConfigContent = await fs.promises.readFile(gitConfigPath, 'utf8');
@@ -22,15 +22,15 @@ export class ConfigStringExtractor {
       if (!remoteOriginMatch) throw new Error('Remote origin URL not found');
 
       // Extract URL from the matched pattern
-      const url = remoteOriginMatch[0];
+      const url = remoteOriginMatch[1];
 
       // Extract organization and repository name from URL
-      const orgRepoRegex = /github\.com\/([^\/]+\/[^\/]+)\.git/;
+      const orgRepoRegex = /github\.com\/([^\/]+\/[^\/]+)\.git/; // Define <org>/<repo> as captured group inside the matching pattern.
       const matches = url.match(orgRepoRegex);
       if (!matches) throw new Error('Invalid remote origin URL');
-
-      // Remove .git
-      const orgRepo = matches[0].replace(/\.git$/, '');
+      
+      // Return the first (and only) captured group in the match which is <org>/<repo>. [0] would be the entire match.
+      const orgRepo = matches[1]; 
 
       return orgRepo;
     } catch (error: any) {
