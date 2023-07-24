47c996b7568ae237ca9f43248901a59e7e232d76
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-07-24 20:13:31 +0200

    #chore A little commenting for our interfaces

--- src/interfaces/GitHubTypes.ts
+++ src/interfaces/GitHubTypes.ts
@@ -1,16 +1,16 @@
 // Versions of a GitHub Registry Package in the Build Image assigned to a Repo of an Org.
-// (n PakageVersions : 1 Repository)
+// (n PackageVersions : 1 Repository)
 export interface PackageVersion {
-  image_name_sha: string; // Unique identifies of a Version
-  tags: string[]; // Metadata.container.tags[], empty if no tags given
-  created_at: string;
-  updated_at: string;
+  image_name_sha: string; // Unique identifier of a Version
+  tags: string[]; // Tags associated with the version, empty if no tags given
+  created_at: string; // Timestamp indicating when the version was created
+  updated_at: string; // Timestamp indicating when the version was last updated
 }
 
 // Helper interface for RegistryOpsOrg Class methods.
 export interface PackageImage {
-  name: string;
+  name: string; // Name of the package image
   repository: {
-    full_name: string;
+    full_name: string; // Full name of the repository associated with the package image
   };
 }
